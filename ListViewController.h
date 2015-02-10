cea8b4c0e67f423c46a9629beeb11bc771856bb5
Sherrie Jones <sj24kt@comcast.net>
2015-02-10 13:47:36 -0700

    completed app

--- ListViewController.h
+++ ListViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  ListViewController.h
 //  DreamCatcher
 //
 //  Created by Sherrie Jones on 2/9/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface ListViewController : UIViewController
 
 @end
-
